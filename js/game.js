2b74283044614a3065caf1d1ad8fedd3b58e9053
Brad McKinney <brad.mckin@gmail.com>
2018-06-22 14:32:51 -1000

    Appended all elements for Start screen

--- js/game.js
+++ js/game.js
@@ -5,9 +5,14 @@ const startScreen = document.createElement('div');
 const startHeader = document.createElement('header');
 const gameTitle = document.createElement('h1');
 const startButton = document.createElement('a');
+const body = document.getElementsByTagName('body')[0];
 startScreen.className = "screen screen-start";
 startScreen.id = "start";
 gameTitle.textContent = "Tic Tac Toe"
 startButton.href = "#";
 startButton.className = "button";
 startButton.textContent = "Start game";
+startScreen.appendChild(startHeader);
+startHeader.appendChild(gameTitle);
+startHeader.appendChild(startButton);
+body.appendChild(startScreen);
