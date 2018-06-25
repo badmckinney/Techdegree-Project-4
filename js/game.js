e2f7e897a817f2cf82aee3e293ee0631c00e5a83
Brad McKinney <brad.mckin@gmail.com>
2018-06-25 13:41:51 -1000

    Programmed mouseover and mouseleave event listeners

--- js/game.js
+++ js/game.js
@@ -19,9 +19,9 @@ startHeader.appendChild(startButton);
 body.appendChild(startScreen);
 
 
-/*===========
-    START
-===========*/
+/*==================
+    START BUTTON
+===================*/
 //When "start" button is clicked, start screen is hidden, revealing game board.
 startButton.addEventListener('click', () => {
   startScreen.style.display = "none";
@@ -31,6 +31,7 @@ startButton.addEventListener('click', () => {
     2P GAME
 ============*/
 //Variables
+//Selects all boxes on the game board
 //Sets player 1 as active for turn 1
 //Sets a turn counter
 const p1 = document.getElementById('player1');
@@ -40,45 +41,51 @@ p1.classList.add('active');
 let turns = 0;
 
 //Programming for a 2-player game
+//Iterates over all boxes
+//Listens for clicks on each box
+//If box is not filled, it will be filled based on players turns
+//Turn counter increments and alternates players turn
 const twoPlayerGame = () => {
   boxes.forEach(box => {
     box.addEventListener('click', () => {
       if (event.target.className == 'box') {
         if (turns % 2 === 0) {
-          event.target.classList.add('box-filled-2');
+          event.target.classList.add('box-filled-1');
           p1.classList.remove('active');
           p2.classList.add('active');
           turns += 1;
         } else if (turns % 2 !== 0) {
-          event.target.classList.add('box-filled-1');
+          event.target.classList.add('box-filled-2');
           p1.classList.add('active');
           p2.classList.remove('active');
           turns += 1;
         }
       }
     });
+
+//Listens for a mouse over each box
+//If the box hasn't been selected it will display the image associated with the player whose turn it is
+    box.addEventListener('mouseover', () => {
+      if (event.target.className == 'box') {
+        if (turns % 2 === 0) {
+          event.target.style.backgroundImage = "url('img/o.svg')"
+        } else if (turns % 2 !== 0) {
+          event.target.style.backgroundImage = "url('img/x.svg')"
+        }
+      }
+    });
+
+//Listens for a mouse to leave each box
+//Removes background image
+    box.addEventListener('mouseleave', () => {
+      if (event.target.className == 'box') {
+          event.target.style.backgroundImage = ""
+        }
+    });
+
+
+
   });
 }
 
 twoPlayerGame();
-
-/*const twoPlayerGame = () => {
-  for (i = 0; i < boxes.length; i += 1) {
-    boxes[i].addEventListener('click', () => {
-      if (!boxes[i].classList.contains('box-filled')) {
-        if (turns % 2 === 0) {
-          boxes[i].classList.add('box-filled-2');
-          p1.classList.remove('active');
-          p2.classList.add('active');
-          turns += 1;
-        } else if (turns % 2 !== 0) {
-          boxes[i].classList.add('box-filled-1');
-          p1.classList.add('active');
-          p2.classList.remove('active');
-        }
-      }
-    });
-  }
-}
-
-twoPlayerGame();*/
